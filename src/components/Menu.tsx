3f41305042a6ff6fb8bcc2c3835ab54c6b5b8b85
working-zima <working.zima@gmail.com>
2024-03-02 15:18:25 +0900

    feat(Menu): 사용자는 식당의 메뉴별로 붙어있는 선택 버튼을 통해 점심 바구니에 음식을 담을 수 있다.

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,5 @@
+import { useLocalStorage } from 'usehooks-ts';
+
 import Food from '../types/Food';
 import MenuItem from './MenuItem';
 
@@ -6,9 +8,15 @@ type MenuProps = {
 }
 
 function Menu({ menu }: MenuProps) {
+  const [selectedFoods, selectFood] = useLocalStorage<Food[]>('cart', []);
+
   if (!menu.length) {
     return (<p>메뉴가 존재하지 않습니다.</p>);
   }
+
+  const handleClickSelectFood = (food: Food) => {
+    selectFood([...selectedFoods, food]);
+  };
 
   return (
     <ul>
@@ -21,6 +29,7 @@ function Menu({ menu }: MenuProps) {
               style={{ marginLeft: '0.5rem' }}
               name={`#${food.name}`}
               type="button"
+              onClick={() => handleClickSelectFood(food)}
             >
               선택
             </button>
